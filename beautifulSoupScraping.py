b6bfef6546cde64b966023c283f7b6edad1809bc
Social-merit <inform.cefn@gmail.com>
2023-10-05 12:39:14 +0200

    initial project

---
+++ beautifulSoupScraping.py
@@ -0,0 +1,22 @@
+# import libs
+
+from bs4 import BeautifulSoup
+import requests
+
+
+
+
+# Beautiful soup: Steps before scraping a website
+
+# 1. Get the url: Fetch the pages (obtained a reponse object)
+
+
+
+# 2. Page content, parse the HTML: Extract the data from the HTML (using the response object)
+
+
+
+# 3. Create Soup, Find the data: by using the methods of the BeautifulSoup object
+
+
+
---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+
+#  Steps before scraping a website
+
+# 1. Get the url: Fetch the pages (obtained a reponse object)
+
+
+
+# 2. Page content, parse the HTML: Extract the data from the HTML (using the response object)
+
+
+
+# 3. Create, Find the data: by using the methods object
+
+
+
