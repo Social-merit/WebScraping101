2fb2c22d8a3216c98c316bc49dbc981230015f92
Social-merit <inform.cefn@gmail.com>
2023-10-05 20:48:37 +0200

    completed beautiful soup

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
+import requests
+import os
+from bs4 import BeautifulSoup
 
 
+# Create a folder to save the PDF
+folder_name = 'PDFs'
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
 
-#  Steps before scraping a website
+# https://appunti.cavallium.it/Algebra%20e%20Geometria/
 
-# 1. Get the url: Fetch the pages (obtained a reponse object)
+root = 'https://appunti.cavallium.it'
+website_url = f'{root}/Algebra%20e%20Geometria'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+}
+
+response = requests.get(website_url, headers=headers)
 
 
+# response = requests.get(website_url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-# 2. Page content, parse the HTML: Extract the data from the HTML (using the response object)
+
+box = soup.find('tbody')
 
 
+pdf_urls = []
+for link in box.find_all('a', href=True):
+    if '.pdf' in link['href']:
+        pdf_urls.append(link['href'])
 
-# 3. Create, Find the data: by using the methods object
+# Download the PDFs using requests
+for i, pdf_url in enumerate(pdf_urls):
+    # result = requests.get(f'{website_url}/{pdf_url}')
+    pdf_response = requests.get(f'{website_url}/{pdf_url}')
+    pdf_file_path = os.path.join(folder_name, f'file_{i + 1}.pdf')  # Save the PDF in the created folder with a unique name
+    with open(pdf_file_path, 'wb') as f:
+        f.write(pdf_response.content)
 
 
 
