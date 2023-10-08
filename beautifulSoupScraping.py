6057602c26be788b301019a787c407f68fe9e86f
Social-merit <inform.cefn@gmail.com>
2023-10-08 13:40:59 +0200

    Completed Part1 Selenium web Scarping

--- beautifulSoupScraping.py
+++ beautifulSoupScraping.py
@@ -102,3 +102,73 @@ for page in range(1, int(last_page)+1):
         except:
             print('------ Link not working -------')
             print(link)
+
+
+
+
+'''
+import requests
+import os
+import time
+from bs4 import BeautifulSoup
+
+# Create a folder to save the PDFs
+folder_name = 'PDFs'
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+
+# Headers to mimic a browser request
+headers = {
+    'User-Agent': 'Mozilla/105.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+   
+}
+
+root = 'https://appunti.cavallium.it'
+# Navigate to the webpage and find the PDF URLs using BeautifulSoup
+website_url = f'{root}/Algebra%20e%20Geometria'
+response = requests.get(website_url, headers=headers)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+box = soup.find('tbody')
+
+print(box)
+
+# Find the PDF URL
+pdf_url = None
+for link in box.find_all('a', href=True):
+    if '.pdf' in link['href']:
+        pdf_url = link['href']
+        print(pdf_url)
+        break
+
+# Download the PDF using requests
+if pdf_url:
+    pdf_response = requests.get(f'{website_url}/{pdf_url}', headers=headers)
+    pdf_file_path = os.path.join(folder_name, 'file.pdf')  # Save the PDF in the created folder
+    with open(pdf_file_path, 'wb') as f:
+        f.write(pdf_response.content)
+
+
+
+# Download multiple PDF using requests
+# pdf_urls = []
+# for link in box.find_all('a', href=True):
+#     if '.pdf' in link['href']:
+#         pdf_urls.append(link['href'])
+
+# # Download the PDFs using requests
+# for i, pdf_url in enumerate(pdf_urls):
+#     pdf_response = requests.get(f'{website_url}/{pdf_url}', headers=headers)
+#     pdf_file_path = os.path.join(folder_name, f'file_{i + 1}.pdf')  # Save the PDF in the created folder with a unique name
+#     with open(pdf_file_path, 'wb') as f:
+#         f.write(pdf_response.content)
+#     time.sleep(5)  # Wait for 2 seconds before the next request
+#     print(f'File {i + 1} downloaded successfully!')
+
+# print('All PDFs downloaded successfully!')
+
+
+
+
+
+'''
---
+++ part1SeleniumWebScraping.py
@@ -0,0 +1,91 @@
+###############################################################################################################
+# ==================  Scraping football data from a website using Selenium and Python part 1 ==================
+###############################################################################################################
+
+
+ # ================================= Importing libraries =================================
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
+import pandas as pd
+import time
+
+
+
+
+ # ================================= Set path for chromedriver.exe  and website =================================
+
+path = './../../../../../Download/CV/chromedriver/chromedriver.exe'
+# Define the website to scrape and path where the chromedriver is located
+website = 'https://www.adamchoi.co.uk/overs/detailed'
+
+
+
+# =============================  Selenium driver initialization  ==========================================
+service = Service(executable_path=path) # Responsible for starting the ChromeDriver server
+driver = webdriver.Chrome(service=service) # instance of chrome driver
+driver.get(website) # to open the website in the browser
+
+
+# ================================== Locate and click on a button ==================================
+# //*[@id="page-wrapper"]/div/div[3]/div/div/div/label[1]
+all_matches_button = driver.find_element('xpath', '//*[@id="page-wrapper"]/div/div[3]/div/div/div/label[1]') # to select over 1.5
+all_matches_button.click() # to click on the button
+
+# //*[@id="page-wrapper"]/div/home-away-selector/div/div/div/div/label[2]
+# all_matches_button = driver.find_element('xpath', '//label[@analytics-event="All matches"]')
+all_matches_button = driver.find_element('xpath', '//*[@id="page-wrapper"]/div/home-away-selector/div/div/div/div/label[2]') # to select All matches
+all_matches_button.click() # to click on the button
+
+
+time.sleep(1)
+
+# (1) England, premier league,  2023-24
+
+
+# ===================   Select dropdown and select element inside by visible text  ===================
+# //*[@id="country"]
+dropdown = Select(driver.find_element('id', 'country')) # to select the dropdown country
+dropdown.select_by_visible_text('England')
+
+# //*[@id="league"]
+dropdown = Select(driver.find_element('id', 'league')) # to select the dropdown league
+dropdown.select_by_visible_text('Premier League')
+
+# //*[@id="season"]
+dropdown = Select(driver.find_element('id', 'season')) # to select the dropdown season
+dropdown.select_by_visible_text('22/23')
+
+
+time.sleep(2)
+
+
+# ============================== from the table to list storage: looping through the table to the matche list ==============================
+
+matches = driver.find_elements('xpath', '//tr') # to select all the matches in the table
+
+date = []
+home_team = []
+score = []
+away_team = []
+
+for match in matches: # looping through the matches
+    date.append(match.find_element('xpath', './td[1]').text) # to select the date of the match in the table
+    home = match.find_element('xpath', './td[2]').text # to select the home team of the match in the table
+    home_team.append(home)
+    score.append(match.find_element('xpath', './td[3]').text) # to select the score of the match in the table
+    away_team.append(match.find_element('xpath', './td[4]').text) # to select the away team of the match in the table
+
+driver.quit() # to close the driver
+
+
+
+# =================================  Create DataFrame in Pandas and export to CSV  =================================
+
+df = pd.DataFrame({'date': date, 'home_team': home_team, 'score': score, 'away_team': away_team}) # to create a DataFrame in Pandas with the data looped from the table in the website
+
+df.to_csv('EnglandPremierLeague202223Football_data.csv', index=False) # to export the DataFrame to a CSV file
+
+
+print('Dataframe created and exported to CSV') # to print a message
+print(df) # to print the DataFrame
